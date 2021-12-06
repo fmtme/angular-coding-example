3a0c82c525ecff071b28a682d793934f26dba4b5
David <david.wyett@ctrlshift.tech>
2021-12-06 11:14:08 +1100

    made list view responsive using breakpoint observer from cdk

--- src/app/person/person-list/person-list.component.ts
+++ src/app/person/person-list/person-list.component.ts
@@ -1,8 +1,9 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {PersonService} from "../person.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
+import {BreakpointObserver , BreakpointState} from "@angular/cdk/layout";
 
 @Component({
     selector: 'app-person-list',
@@ -14,18 +15,28 @@ export class PersonListComponent implements OnInit, AfterViewInit {
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
 
-
     // using MatTableDataSource for this demo app. It allows for client side filter, sort, pagination.
     // in a real app these are things we'd probably do on server
     dataSource: MatTableDataSource<any>;
     displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email']; // this determines visual order, not ng-container in template
+    isDesktop: boolean;
 
-    constructor(private personService: PersonService) {
-    }
+    constructor(private personService: PersonService, private breakpointObserver: BreakpointObserver) {}
 
     ngOnInit(): void {
         this.dataSource = new MatTableDataSource(this.personService.getAll())
         console.log(this.dataSource)
+        this.breakpointObserver
+            .observe(['(min-width: 700px)'])
+            .subscribe((state: BreakpointState) => {
+                if (state.matches) {
+                    console.log('Viewport width is 500px or greater!');
+                    this.isDesktop = true;
+                } else {
+                    console.log('Viewport width is less than 500px!');
+                    this.isDesktop = false
+                }
+            });
     }
 
     ngAfterViewInit(): void {
@@ -41,5 +52,4 @@ export class PersonListComponent implements OnInit, AfterViewInit {
         const filterValue = (event.target as HTMLInputElement).value;
         this.dataSource.filter = filterValue.trim().toLowerCase();
     }
-
 }
